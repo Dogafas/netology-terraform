f3d8c2917fc887ccd2a27cc794ef1292e444ed14
Dogafas <solomindavid10@gmail.com>
2026-01-22 13:44:55 +0700

    Добав. файл outputs.tf с выводом информации о VMs и файл readme.md с примером вывода команды terraform output

---
+++ 02.4/outputs.tf
@@ -0,0 +1,17 @@
+output "instances_info" {
+  description = "Name, external IP and FQDN of all VMs"
+
+  value = {
+    web = {
+      name        = yandex_compute_instance.platform.name
+      external_ip = yandex_compute_instance.platform.network_interface[0].nat_ip_address
+      fqdn        = yandex_compute_instance.platform.fqdn
+    }
+
+    db = {
+      name        = yandex_compute_instance.platform_db.name
+      external_ip = yandex_compute_instance.platform_db.network_interface[0].nat_ip_address
+      fqdn        = yandex_compute_instance.platform_db.fqdn
+    }
+  }
+}
---
+++ 02.4/readme.md
@@ -0,0 +1,19 @@
+**Вывод значений ip-адресов команды terraform output**
+
+```
+meteoguru@Z68P-DS3:~/DEVELOP/netology-terraform/02.4$ terraform output
+instances_info = {
+  "db" = {
+    "external_ip" = "89.169.178.207"
+    "fqdn" = "epdpahlj2djina3dgp8l.auto.internal"
+    "name" = "netology-develop-platform-db"
+  }
+  "web" = {
+    "external_ip" = "178.154.240.53"
+    "fqdn" = "fhmbd6f0i0g7q1kofidd.auto.internal"
+    "name" = "netology-develop-platform-web"
+  }
+}
+meteoguru@Z68P-DS3:~/DEVELOP/netology-terraform/02.4$ 
+
+```
