bb98dc119803d4c48d0100b5eb33e6ae85c94ea9
ByteBard <solomindavid10@gmail.com>
2026-01-25 18:13:00 +0700

    Добав. заготовка для задания 4

---
+++ 04/for_each-vm.tf
@@ -0,0 +1,41 @@
+locals {
+  each_vm_map = {
+    for vm in var.each_vm :
+    vm.vm_name => vm
+  }
+}
+
+resource "yandex_compute_instance" "db" {
+  for_each = local.each_vm_map
+
+  name = "db-${each.value.vm_name}"
+
+  platform_id = "standard-v1"
+  zone        = var.default_zone
+
+  resources {
+    cores  = each.value.cpu
+    memory = each.value.ram
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.id
+      size     = each.value.disk_volume
+    }
+  }
+
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.develop.id
+    nat                = true
+    security_group_ids = [yandex_vpc_security_group.example.id]
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${local.ssh_public_key}"
+  }
+}
+
+data "yandex_compute_image" "ubuntu" {
+  family = "ubuntu-2004-lts"
+}
